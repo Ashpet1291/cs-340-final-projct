da11624a7394c3a2bc6f1874e2404fc2ce843c6e
muchm001@gmail.com <Ender1576!>
2021-08-02 17:30:21 -0500

    MJU

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const path = require("path");
 
-/*
-    SETUP
-*/
-var express = require('express');
-var app = express();
-PORT = 32360;
+const app = express();
+const port = 12560;
 
-/*
-    ROUTES
-*/
-app.get('/', function(req, res)
-{
-    res.send("The server is running!")
+
+app.engine('hbs', hbs({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: __dirname + '/views/partials/',
+}));
+
+app.set('view engine', 'hbs');
+
+
+app.use(express.static('img'));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'});
 });
 
-/*
-    LISTENER
-*/
-app.listen(PORT, function(){
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+app.get('/thinkers', (req, res) => {
+    res.render('thinkers', {layout: 'index'});
 });
+
+app.get('/philosophy', (req, res) => {
+    res.render('philosophy', {layout: 'index'});
+});
+
+app.get('/orgs', (req, res) => {
+    res.render('orgs', {layout: 'index'});
+});
+
+app.use(function(req, res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.log(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
